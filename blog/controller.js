4ab4c3e7d410e189f21bc2d371c6be8d6af7ddae
ethien <ethienduckett@outlook.com>
2022-04-21 14:55:30 -0400

    I created working page navigation for the blogs (I think) I rearranged the sequence of items in the root index.html file

--- blog/controller.js
+++ blog/controller.js
@@ -16,10 +16,18 @@ function nextPage(){
 	let count = document.getElementById("blog_index");
 	let index = Number(count.index);
 	let blogs = document.getElementsByClassName("blog");
+	if (window.lastPaging == "prev") {
+		index += blogs.length;
+		if (index >= blogs.length) {
+			index=blogs.length;
+		}
+	}
+	window.lastPaging = "next";
 	if (index >= blogs.length) {
 		alert("There are no more blogs left");
 		return;
 	}
+	const re = new RegExp("(?<=\/).*");
 	for (const blog of blogs) {
 		let name = Object.keys(window.database)[index];
 		let blog_data = window.database[name];
@@ -28,13 +36,55 @@ function nextPage(){
 		blog.children[1].innerHTML = blog_data["hook"];
 		blog.children[2].children[0].innerHTML = blog_data["creation_time"];
 		
-		blog.children[2].children[1].innerHTML = `<a>${blog_data["start_file"]}</a>`;
+		let start_file = blog_data["start_file"];
+		blog.children[2].children[1].innerHTML = `<a href="${start_file}">${re.exec(start_file)}</a>`;
 		index++;
 		if (index >= blogs.length) {
 			count.index = String(index);
 			return;
 		}
+	}
+	count.index = String(index);
+}
 
+function prevPage(){
+	let count = document.getElementById("blog_index");
+	let index = Number(count.index);
+	let blogs = document.getElementsByClassName("blog");
+	if (window.lastPaging == "next") {
+		index -= blogs.length;
+		if (index < 0) {
+			index=0;
+		}
+	}
+	window.lastPaging = "prev";
+	console.log("prevPage()");
+	if (index-blogs.length+1 <= 0) {
+		alert("There are no more blogs left");
+		return;
+	}
+	if (index >= blogs.length) {
+		index--;
+	}
+	const re = new RegExp("(?<=\/).*");
+	let blog;
+	for (let i = blogs.length-1; i >= 0; i--) {
+		console.log("iteration");
+		blog = blogs[i];
+		let name = Object.keys(window.database)[index];
+		let blog_data = window.database[name];
+
+		blog.children[0].innerHTML = name;
+		blog.children[1].innerHTML = blog_data["hook"];
+		blog.children[2].children[0].innerHTML = blog_data["creation_time"];
+		let start_file = blog_data["start_file"];
+		blog.children[2].children[1].innerHTML = `<a href="${start_file}">${re.exec(start_file)}</a>`;
+		index--;
+		if (index <= 0) {
+			index = 0
+			count.index = String(index);
+			return;
+		}
 	}
 	count.index = String(index);
 }
